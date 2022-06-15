01f76bc9fe349464030f00b8446c91d4ed504b8e
kiritocode1 <kathawalearyan9@gmail.com>
2022-06-15 20:23:37 +0530

    added basic things like colour etc

--- src/App.js
+++ src/App.js
@@ -3,10 +3,13 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <h1> Work in progress. coding with ❤ </h1>
+    <div className=" App white">
+      <h2 className={`mono_text2`}> Work in progress. coding with <b classname="red">❤</b> </h2>
+            <h1 className={`japan_text1 red`}> 
+        愛している </h1>
       
     </div>
+
   );
 }
 
