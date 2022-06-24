b818cbe2e0af40d22171ecf62f4a2be7d55c8eed
kiritocode1 <kathawalearyan9@gmail.com>
2022-06-24 17:01:28 +0530

    done adding the stuff:

---
+++ src/components/photos/index.jsx
@@ -0,0 +1,8 @@
+import react from "react";
+const component = () => {
+    return (<>
+    
+        <h1>hello world this is an example code for some friends </h1>
+    </>)
+};
+export default component; 
