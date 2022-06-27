4a94316ffd85c5c2f96787df39f6029688c9c536
kiritocode1 <kathawalearyan9@gmail.com>
2022-06-28 02:14:46 +0530

    completed react spring stuff

--- src/App.js
+++ src/App.js
@@ -1,12 +1,19 @@
+import { useSpring, animated as a } from "react-spring";
+import { useState } from "react";
 // import logo from './logo.svg';
 import './App.css';
 // import { useSpring,animated as a } from "react-spring";
 function App() {
+  const [butt, setButt] = useState(true);
+  const stykle = useSpring({
+    backgroundColor: butt ? "red" : "white"
+  })
   return (
     <div className='App white'>
-      <div className=' mono_text2 animate-pulse'>Work in progress , made W/ 🖤 </div>
+      <div className=' mono_text2 animate-pulse'>Work in progress , made W/ 🖤 </div>       
       <div className="japan_text1 text-red-700 justify-center">愛している </div>
-      {/* <div style={}/> */}
+      <a.div style={stykle} className="w-[20px] h-[20px] rounded-xl" />
+      <button onClick={setButt(!butt)}></button>
     </div>
 
   );
