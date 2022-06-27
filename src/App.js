13515087274c6f665c4ca0b0700a34bb9881a344
kiritocode1 <kathawalearyan9@gmail.com>
2022-06-28 02:28:25 +0530

    added stuff for experimentation

--- src/App.js
+++ src/App.js
@@ -1,19 +1,28 @@
 import { useSpring, animated as a } from "react-spring";
-import { useState } from "react";
+import { useState,useEffect as ue } from "react";
 // import logo from './logo.svg';
 import './App.css';
 // import { useSpring,animated as a } from "react-spring";
 function App() {
-  const [butt, setButt] = useState(true);
+  const [col, setcol] = useState(true);
+  const func = () => {
+    setcol(!col);
+  };
   const stykle = useSpring({
-    backgroundColor: butt ? "red" : "white"
-  })
+    from: {
+    backgroundColor: "red",
+      
+    },
+    backgroundColor: "white",
+    loop:true,
+  });
+  
   return (
     <div className='App white'>
       <div className=' mono_text2 animate-pulse'>Work in progress , made W/ 🖤 </div>       
       <div className="japan_text1 text-red-700 justify-center">愛している </div>
       <a.div style={stykle} className="w-[20px] h-[20px] rounded-xl" />
-      <button onClick={setButt(!butt)}></button>
+      <button onClick={func}>clickme</button>
     </div>
 
   );
