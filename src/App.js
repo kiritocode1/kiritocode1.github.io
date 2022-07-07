6264657042f77ec213065fa06cdd45f1a8622f70
kiritocode1 <kathawalearyan9@gmail.com>
2022-07-07 23:46:59 +0530

    randomise done

--- src/App.js
+++ src/App.js
@@ -1,34 +1,32 @@
 import { useSpring, animated as a } from "react-spring";
 import {useState,useEffect as UseE} from "react";
 import './App.css';
-
+import randomize from "./components/Randomize";
 
 import Home from "./components/Home/home";
 
 function App() {
   const [col, setcol] = useState(false);
-  const [insert, setint] = useState([[1, 0, 0], [0, 1, 0], [0, 0, 1]]);
+  // const [insert, setint] = useState([[1, 0, 0], [0, 1, 0], [0, 0, 1]]);
   const stykle = useSpring({
     backgroundColor:col?"red":"white"
   });
-  const randomise_btwn_01 = (min, max) => {
-    return Math.floor(Math.random() * (max - min + 1) ) + min;
-  };
   UseE(() => {
-    randomise_btwn_01();
+    const a = randomize();
+    console.log(a);
   }, [col]);
   return (
     <div className='App' >
       <div className={`${col?"white":""}`}>
-      <Home/>
       <div className=' mono_text2 animate-pulse'>Work in progress , made W/ {!col?"🖤":"❤️👹"} </div>       
       <div className="japan_text1 text-red-700 justify-center">愛している </div>
       <div className="w-full flex items-center justify-center">
-      <a.div style={stykle} className="w-[10px] h-[10px] rounded-xl" /></div>
+      <a.div style={stykle} className="w-[10px] h-[10px] rounded-full" /></div>
       <button onClick={()=>setcol(!col)} className="font-mono text-2xl">clickity</button>
         <div>{col ? 'HELLO' : "WORLD"}</div>
-        <div>{randomise_btwn_01(0, 1)}</div>
-        <div>{insert}</div>
+        <div>{randomize(0, 1)}</div>
+        {/* <div>{insert}</div> */}
+      <Home/>
       </div>
       </div>
   );
