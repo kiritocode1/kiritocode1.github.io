ef64c533b98afc9808ef4e7c5c6277ffe34f63b7
kiritocode1 <kathawalearyan9@gmail.com>
2022-07-08 18:21:12 +0530

    completed compiling home

--- src/App.js
+++ src/App.js
@@ -1,23 +1,24 @@
-import { useSpring, animated as a } from "react-spring";
-import {useState,useEffect as UseE} from "react";
+// import { useSpring, animated as a } from "react-spring";
+// import {useState,useEffect as UseE} from "react";
 import './App.css';
-import randomize from "./components/Randomize";
+// import randomize from "./components/Randomize";
 
 import Home from "./components/Home/home";
+import { Routes, Route } from "react-router-dom";
 
 function App() {
-  const [col, setcol] = useState(false);
-  // const [insert, setint] = useState([[1, 0, 0], [0, 1, 0], [0, 0, 1]]);
-  const stykle = useSpring({
-    backgroundColor:col?"red":"white"
-  });
-  UseE(() => {
-    const a = randomize();
-    console.log(a);
-  }, [col]);
+  // const [col, setcol] = useState(false);
+  // // const [insert, setint] = useState([[1, 0, 0], [0, 1, 0], [0, 0, 1]]);
+  // const stykle = useSpring({
+  //   backgroundColor:col?"red":"white"
+  // });
+  // UseE(() => {
+  //   const a = randomize();
+  //   console.log(a);
+  // }, [col]);
   return (
     <div className='App' >
-      <div className={`${col?"white":""}`}>
+      {/* <div className={`${col?"white":""}`}>
       <div className=' mono_text2 animate-pulse'>Work in progress , made W/ {!col?"🖤":"❤️👹"} </div>       
       <div className="japan_text1 text-red-700 justify-center">愛している </div>
       <div className="w-full flex items-center justify-center">
@@ -25,9 +26,22 @@ function App() {
       <button onClick={()=>setcol(!col)} className="font-mono text-2xl">clickity</button>
         <div>{col ? 'HELLO' : "WORLD"}</div>
         <div>{randomize(0, 1)}</div>
-        {/* <div>{insert}</div> */}
+
       <Home/>
-      </div>
+      </div> */}
+      
+      <Routes>
+        <Route path='/' element={<Home />} />
+        
+      </Routes>
+
+
+
+
+
+
+
+
       </div>
   );
 }
--- src/components/photos/index.jsx
+++
@@ -1,8 +0,0 @@
-import react from "react";
-const component = () => {
-    return (<>
-    
-        <h1>hello world this is an example code for some friends </h1>
-    </>)
-};
-export default component; 
