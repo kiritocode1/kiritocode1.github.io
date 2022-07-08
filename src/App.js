b62719f7881053db83508a0b2dcbfa03b281b82f
kiritocode1 <kathawalearyan9@gmail.com>
2022-07-09 02:01:51 +0530

    completed re integrating

--- src/App.js
+++ src/App.js
@@ -1,24 +1,24 @@
-// import { useSpring, animated as a } from "react-spring";
-// import {useState,useEffect as UseE} from "react";
+import { useSpring, animated as a } from "react-spring";
+import {useState,useEffect as UseE} from "react";
 import './App.css';
-//   import randomize from "./components/Randomize";
+  import randomize from "./components/Randomize";
     
 import Home from "./components/Home/home";
 import { Routes, Route, Link } from "react-router-dom";
 import Music from "./components/Music/index";
 function App() {
-  // const [col, setcol] = useState(false);
-  // // const [insert, setint] = useState([[1, 0, 0], [0, 1, 0], [0, 0, 1]]);
-  // const stykle = useSpring({
-  //   backgroundColor:col?"red":"white"
-  // });
-  // UseE(() => {
-  //   const a = randomize();
-  //   console.log(a);
-  // }, [col]);
+  const [col, setcol] = useState(false);
+  // const [insert, setint] = useState([[1, 0, 0], [0, 1, 0], [0, 0, 1]]);
+  const stykle = useSpring({
+    backgroundColor:col?"red":"white"
+  });
+  UseE(() => {
+    const a = randomize();
+    console.log(a);
+  }, [col]);
   return (
     <div className='App' >
-      {/* <div className={`${col?"white":""}`}>
+      <div className={`${col?"white":""}`}>
       <div className=' mono_text2 animate-pulse'>Work in progress , made W/ {!col?"🖤":"❤️👹"} </div>       
       <div className="japan_text1 text-red-700 justify-center">愛している </div>
       <div className="w-full flex items-center justify-center">
@@ -28,7 +28,7 @@ function App() {
         <div>{randomize(0, 1)}</div>
 
       <Home/>
-      </div> */}
+      </div>
       <h1>hello wold</h1>
       <Routes>
         <Route path='/' element={<Home />} />
