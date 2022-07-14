c4f89450cab8ebea11eeb3bdd32eda1edfe3547b
kiritocode1 <kathawalearyan9@gmail.com>
2022-07-14 22:31:03 +0530

    copy

--- src/App.js
+++ src/App.js
@@ -6,6 +6,11 @@ import './App.css';
 // import Home from "./components/Home/home";
 // import { Routes, Route, Link } from "react-router-dom";
 // import Music from "./components/Music/index";
+
+
+import { SiMedium } from "react-icons/si";
+import { FaGithub } from "react-icons/fa";
+import { BsLinkedin } from "react-icons/bs";
 function App() {
   // const [col, setcol] = useState(false);
   // // const [insert, setint] = useState([[1, 0, 0], [0, 1, 0], [0, 0, 1]]);
@@ -23,6 +28,10 @@ function App() {
     <div className='App ' >
       <h1 className='text-3xl font-mono animate-pulse'>Coding with love 🖤</h1>
       <div className='w-full flex  justify-center py-20'>
+
+
+
+
       <div className="grid grid-flow-col gap-5 text-center auto-cols-max">
   <div className="flex flex-col">
     <span className="countdown font-mono text-5xl">
@@ -51,8 +60,32 @@ function App() {
       </div>
       </div>
       <h1>UI themes :</h1>
+
+
+
+
+
+      
       <div className="w-full flex items-center justify-evenly">
-      <label className="swap swap-rotate">
+      
+        <div class="rating">
+  <input type="radio" name="rating-2" class="mask mask-star-2 bg-orange-400" />
+  <input type="radio" name="rating-2" class="mask mask-star-2 bg-orange-400"  />
+  <input type="radio" name="rating-2" class="mask mask-star-2 bg-orange-400" />
+  <input type="radio" name="rating-2" class="mask mask-star-2 bg-orange-400" />
+  <input type="radio" name="rating-2" class="mask mask-star-2 bg-orange-400" />
+</div>
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        <label className="swap swap-rotate">
   
   <input type="checkbox" />
   
@@ -121,10 +154,12 @@ function App() {
     <p>Copyright © 2022 - All right reserved by <b className='font-mono'>kiritocode1</b> on <b>Github Pages 🖤</b></p>
   </div> 
   <div class="grid-flow-col gap-4 md:place-self-center md:justify-self-end">
-    <a href='google.com'><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" class="fill-current"><path d="M24 4.557c-.883.392-1.832.656-2.828.775 1.017-.609 1.798-1.574 2.165-2.724-.951.564-2.005.974-3.127 1.195-.897-.957-2.178-1.555-3.594-1.555-3.179 0-5.515 2.966-4.797 6.045-4.091-.205-7.719-2.165-10.148-5.144-1.29 2.213-.669 5.108 1.523 6.574-.806-.026-1.566-.247-2.229-.616-.054 2.281 1.581 4.415 3.949 4.89-.693.188-1.452.232-2.224.084.626 1.956 2.444 3.379 4.6 3.419-2.07 1.623-4.678 2.348-7.29 2.04 2.179 1.397 4.768 2.212 7.548 2.212 9.142 0 14.307-7.721 13.995-14.646.962-.695 1.797-1.562 2.457-2.549z"></path></svg>
+    <a href='https://twitter.com/kirito08922920'><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" class="fill-current"><path d="M24 4.557c-.883.392-1.832.656-2.828.775 1.017-.609 1.798-1.574 2.165-2.724-.951.564-2.005.974-3.127 1.195-.897-.957-2.178-1.555-3.594-1.555-3.179 0-5.515 2.966-4.797 6.045-4.091-.205-7.719-2.165-10.148-5.144-1.29 2.213-.669 5.108 1.523 6.574-.806-.026-1.566-.247-2.229-.616-.054 2.281 1.581 4.415 3.949 4.89-.693.188-1.452.232-2.224.084.626 1.956 2.444 3.379 4.6 3.419-2.07 1.623-4.678 2.348-7.29 2.04 2.179 1.397 4.768 2.212 7.548 2.212 9.142 0 14.307-7.721 13.995-14.646.962-.695 1.797-1.562 2.457-2.549z"></path></svg>
     </a> 
-    <a href='google.com'><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" class="fill-current"><path d="M19.615 3.184c-3.604-.246-11.631-.245-15.23 0-3.897.266-4.356 2.62-4.385 8.816.029 6.185.484 8.549 4.385 8.816 3.6.245 11.626.246 15.23 0 3.897-.266 4.356-2.62 4.385-8.816-.029-6.185-.484-8.549-4.385-8.816zm-10.615 12.816v-8l8 3.993-8 4.007z"></path></svg></a>
-    <a href='google'><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" class="fill-current"><path d="M9 8h-3v4h3v12h5v-12h3.642l.358-4h-4v-1.667c0-.955.192-1.333 1.115-1.333h2.885v-5h-3.808c-3.596 0-5.192 1.583-5.192 4.615v3.385z"></path></svg></a>
+
+        <a href='https://www.facebook.com/aryan.kathawale.16/' className=" w-8 h-8"><FaGithub className="w-full h-6"/></a>
+          <a href='https://www.facebook.com/aryan.kathawale.16/' className=" w-8 h-8"><SiMedium className="w-full h-6" /></a>
+            <a href='https://www.facebook.com/aryan.kathawale.16/' className=" w-8 h-8"><BsLinkedin className="w-full h-6"/></a>
   </div>
 </footer>
 
