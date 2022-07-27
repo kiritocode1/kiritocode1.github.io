586c88b11fb268f27790b5e278199fc3d6eae5cf
kiritocode1 <kathawalearyan9@gmail.com>
2022-07-27 14:18:47 +0530

    commit favicon added > still not sure

--- src/App.js
+++ src/App.js
@@ -10,10 +10,14 @@ import { BsLinkedin } from "react-icons/bs";
 
 
 
-
+//? Random Starry_sky Generator
 import Stars from './components/display_stars/stars';
 
+// ? DAY-Night Theme Switch . lol .
+// import ModeSwap from './components/light_dark';
 
+//? Rating 5 ⭐
+import RateFront from './components/Rating_system/Rate_front';
 
 
 function App() {
@@ -31,36 +35,19 @@ function App() {
 {/* first ui  */}
       
       <div className="w-full       md:flex md:flex-row flex flex-col  gap-9 items-center justify-evenly">
-      
-        <div class="rating">
-  <input type="radio" name="rating-2" class="mask mask-star-2 bg-sky-400" />
-  <input type="radio" name="rating-2" class="mask mask-star-2 bg-sky-400"  />
-  <input type="radio" name="rating-2" class="mask mask-star-2 bg-sky-400" />
-  <input type="radio" name="rating-2" class="mask mask-star-2 bg-sky-400" />
-  <input type="radio" name="rating-2" class="mask mask-star-2 bg-sky-400"  />
-</div>
+ 
         
         
         
         
         
+        <RateFront/>
+        
+
         
         
-        
-        
-        
-        <label className="swap swap-rotate">
-  
-  <input type="checkbox" />
-  
-  
-  <svg className="swap-on fill-current w-10 h-10" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><path d="M5.64,17l-.71.71a1,1,0,0,0,0,1.41,1,1,0,0,0,1.41,0l.71-.71A1,1,0,0,0,5.64,17ZM5,12a1,1,0,0,0-1-1H3a1,1,0,0,0,0,2H4A1,1,0,0,0,5,12Zm7-7a1,1,0,0,0,1-1V3a1,1,0,0,0-2,0V4A1,1,0,0,0,12,5ZM5.64,7.05a1,1,0,0,0,.7.29,1,1,0,0,0,.71-.29,1,1,0,0,0,0-1.41l-.71-.71A1,1,0,0,0,4.93,6.34Zm12,.29a1,1,0,0,0,.7-.29l.71-.71a1,1,0,1,0-1.41-1.41L17,5.64a1,1,0,0,0,0,1.41A1,1,0,0,0,17.66,7.34ZM21,11H20a1,1,0,0,0,0,2h1a1,1,0,0,0,0-2Zm-9,8a1,1,0,0,0-1,1v1a1,1,0,0,0,2,0V20A1,1,0,0,0,12,19ZM18.36,17A1,1,0,0,0,17,18.36l.71.71a1,1,0,0,0,1.41,0,1,1,0,0,0,0-1.41ZM12,6.5A5.5,5.5,0,1,0,17.5,12,5.51,5.51,0,0,0,12,6.5Zm0,9A3.5,3.5,0,1,1,15.5,12,3.5,3.5,0,0,1,12,15.5Z"/></svg>
-  
-    
-  <svg className="swap-off fill-current w-10 h-10" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><path d="M21.64,13a1,1,0,0,0-1.05-.14,8.05,8.05,0,0,1-3.37.73A8.15,8.15,0,0,1,9.08,5.49a8.59,8.59,0,0,1,.25-2A1,1,0,0,0,8,2.36,10.14,10.14,0,1,0,22,14.05,1,1,0,0,0,21.64,13Zm-9.5,6.69A8.14,8.14,0,0,1,7.08,5.22v.27A10.15,10.15,0,0,0,17.22,15.63a9.79,9.79,0,0,0,2.1-.22A8.11,8.11,0,0,1,12.14,19.73Z"/></svg>
-  
-      </label>
       <input type="checkbox" className="toggle toggle-accent"  />
+      
       
 <label for="my-modal-6" className="btn modal-button">open modal</label>
 
@@ -74,7 +61,12 @@ function App() {
       <label for="my-modal-6" className="btn">Yay!</label>
     </div>
   </div>
-      </div>
+        </div>
+        
+
+
+
+
       </div>
 
       
@@ -120,7 +112,8 @@ function App() {
 <div class="mockup-code">
   <pre data-prefix="$"><code className="animate-pulse">npx kiritocode1</code></pre>
   <pre data-prefix=">" class="text-warning"><code>for easter egg</code></pre>
-</div>
+        </div>
+        
       </div>
       <div className='my-20  w-full    md:flex md:flex-row flex flex-col items-center justify-evenly md:h-96 gap-10   '>
         <div class="radial-progress text-primary" style={{ "--value": 70 , "--size":'12rem'}}>70%</div>
