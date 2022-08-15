5cfb6a7ab630cabf531d20ace2e1c18505535768
kiritocode1 <kathawalearyan9@gmail.com>
2022-08-15 12:09:09 +0530

    working tree clean >>> projects page

--- src/Pages/ProjectsPage.jsx
+++ src/Pages/ProjectsPage.jsx
@@ -29,24 +29,15 @@ import {DiMongodb,} from "react-icons/di"
 import {SiExpress,SiRedux,SiDeno,SiAmazonaws,SiTypescript,SiSvelte,SiVercel,SiCplusplus,SiFlutter,SiIonic,SiRedis} from "react-icons/si"
 import {GrMysql} from "react-icons/gr"
 
-const Dropdown = ({child , Language , context}) => {
+
+
+const CoolBtn = ({ element }) => {
   return (
-    <div class="dropdown">
-  <label tabindex="0" class="btn btn-ghost btn-circle w-20 h-20 md:m-2">
-    {child}
-  </label>
-  <div tabindex="0" class="dropdown-content card card-compact w-64 p-2 shadow bg-primary text-primary-content">
-    <div class="card-body">
-      <h3 class="card-title text-3xl heroMarkup">{Language}</h3>
-      <p>{context}</p>
-    </div>
-  </div>
-</div>
-  );
+    <a.div className="btn btn-ghost hover:text-primary btn-circle md:w-20 md:h-20 md:text-4xl">
+      {element}
+    </a.div>
+  )
 }
-
-
-
 
 
 
@@ -72,38 +63,39 @@ const ProjectsPage = () => {
         }}><img src={sonicRing} alt="SonicRing" className="w-8  " /></div>
         <a.div style={moneyText}   className="text-transparent bg-clip-text  text-9xl  skills bg-primary">Skills</a.div>
       </div>
-      <div className="w-full  flex flex-wrap justify-center items-center">
-        <Dropdown child={<FaHtml5       className='w-8 md:w-12 h-8 md:h-12 text-neutral hover:text-primary focus:text-primary'/>} Language="google" context="google"/>
-        <Dropdown child={<FaCss3Alt        className='w-8 md:w-12 h-8 md:h-12 text-neutral hover:text-primary focus:text-primary'/>} Language="google" context="google" />
-        <Dropdown child={<FaSass       className='w-8 md:w-12 h-8 md:h-12 text-neutral hover:text-primary focus:text-primary'/>} Language="google" context="google"/>
-        <Dropdown child={<FaJsSquare       className='w-8 md:w-12 h-8 md:h-12 text-neutral hover:text-primary focus:text-primary'/>} Language="google" context="google"/>
-        <Dropdown child={<FaMarkdown       className='w-8 md:w-12 h-8 md:h-12 text-neutral hover:text-primary focus:text-primary'/>} Language="google" context="google"/>
-        <Dropdown child={<FaNodeJs       className='w-8 md:w-12 h-8 md:h-12 text-neutral hover:text-primary focus:text-primary'/>} Language="google" context="google"/>
-        <Dropdown child={<FaPython       className='w-8 md:w-12 h-8 md:h-12 text-neutral hover:text-primary focus:text-primary'/>} Language="google" context="google"/>
-        <Dropdown child={<FaReact       className='w-8 md:w-12 h-8 md:h-12 text-neutral hover:text-primary focus:text-primary'/>} Language="google" context="google"/>
-        <Dropdown child={<FaVuejs       className='w-8 md:w-12 h-8 md:h-12 text-neutral hover:text-primary focus:text-primary'/>} Language="google" context="google"/>
-        <Dropdown child={<FaGitSquare       className='w-8 md:w-12 h-8 md:h-12 text-neutral hover:text-primary focus:text-primary'/>} Language="google" context="google"/>
-        <Dropdown child={<FaGithub       className='w-8 md:w-12 h-8 md:h-12 text-neutral hover:text-primary focus:text-primary'/>} Language="google" context="google"/>
-        <Dropdown child={<FaGitlab       className='w-8 md:w-12 h-8 md:h-12 text-neutral hover:text-primary focus:text-primary'/>} Language="google" context="google"/>
-        <Dropdown child={<FaAngular       className='w-8 md:w-12 h-8 md:h-12 text-neutral hover:text-primary focus:text-primary'/>} Language="google" context="google"/>
-        <Dropdown child={<SiExpress       className='w-8 md:w-12 h-8 md:h-12 text-neutral hover:text-primary focus:text-primary'/>} Language="google" context="google"/>
-        <Dropdown child={<DiMongodb       className='w-8 md:w-12 h-8 md:h-12 text-neutral hover:text-primary focus:text-primary'/>} Language="google" context="google"/>
-        <Dropdown child={<SiRedis       className='w-8 md:w-12 h-8 md:h-12 text-neutral hover:text-primary focus:text-primary'/>} Language="google" context="google"/>
-        <Dropdown child={<SiRedux       className='w-8 md:w-12 h-8 md:h-12 text-neutral hover:text-primary focus:text-primary'/>} Language="google" context="google"/>
-        <Dropdown child={<FaPhp       className='w-8 md:w-12 h-8 md:h-12 text-neutral hover:text-primary focus:text-primary'/>} Language="google" context="google"/>
-        <Dropdown child={<SiAmazonaws       className='w-8 md:w-12 h-8 md:h-12 text-neutral hover:text-primary focus:text-primary'/>} Language="google" context="google"/>
-        <Dropdown child={<FaGoogle       className='w-8 md:w-12 h-8 md:h-12 text-neutral hover:text-primary focus:text-primary'/>} Language="google" context="google"/>
-        <Dropdown child={<FaAndroid       className='w-8 md:w-12 h-8 md:h-12 text-neutral hover:text-primary focus:text-primary'/>} Language="google" context="google"/>
-        <Dropdown child={<SiDeno className='w-8 md:w-12 h-8 md:h-12 text-neutral hover:text-primary focus:text-primary' />} Language="google" context="google" />
-        <Dropdown child={<SiSvelte className='w-8 md:w-12 h-8 md:h-12 text-neutral hover:text-primary focus:text-primary' />} Language="google" context="google" /> 
-        <Dropdown child={<SiVercel className='w-8 md:w-12 h-8 md:h-12 text-neutral hover:text-primary focus:text-primary' />} Language="google" context="google" />     
-        <Dropdown child={<SiFlutter className='w-8 md:w-12 h-8 md:h-12 text-neutral hover:text-primary focus:text-primary' />} Language="google" context="google" />  
+
+      <div className="w-full   flex flex-wrap px-4 justify-center items-start">
+        <CoolBtn element={<FaHtml5 />} />
+        <CoolBtn element={<FaCss3Alt />} />
+        <CoolBtn element={<FaSass />} />
+        <CoolBtn element={<FaJsSquare />} />
+        <CoolBtn element={<SiTypescript />} />
+        <CoolBtn element={<SiDeno />} />
+        <CoolBtn element={<SiIonic />} />
+        <CoolBtn element={<SiVercel />} />
+        <CoolBtn element={<SiSvelte />} />
+        <CoolBtn element={<SiAmazonaws />} />
+        <CoolBtn element={<FaNodeJs />} />
+        <CoolBtn element={<FaMarkdown />} />
+        <CoolBtn element={<FaPython />} />
+        <CoolBtn element={<FaReact />} />
+        <CoolBtn element={<FaVuejs />} />
+        <CoolBtn element={<DiMongodb />} />
+        <CoolBtn element={<SiExpress />} />
+        <CoolBtn element={<SiRedux />} />
+        <CoolBtn element={<FaAngular />} />
+        <CoolBtn element={<FaAndroid />} />
+        <CoolBtn element={<FaGitSquare />} />
+        <CoolBtn element={<FaGithub />} />
+        <CoolBtn element={<FaGoogle />} />
+        <CoolBtn element={<SiCplusplus />} />
+        <CoolBtn element={<FaGitlab />} />
+        <CoolBtn element={<FaPhp />} />
+        <CoolBtn element={<SiRedis />} />
+        <CoolBtn element={<SiFlutter />} />
+        <CoolBtn element={<GrMysql />} />
         
-       <Dropdown child={<SiTypescript className='w-8 md:w-12 h-8 md:h-12 text-neutral hover:text-primary focus:text-primary' />} Language="google" context="google" />
-      <Dropdown child={<SiCplusplus className='w-8 md:w-12 h-8 md:h-12 text-neutral hover:text-primary focus:text-primary' />} Language="google" context="google" />        
-      <Dropdown child={<SiIonic className='w-8 md:w-12 h-8 md:h-12 text-neutral hover:text-primary focus:text-primary' />} Language="google" context="google" />        
-      <Dropdown child={<GrMysql className='w-8 md:w-12 h-8 md:h-12 text-neutral hover:text-primary focus:text-primary' />} Language="google" context="google" />        
-</div>
+      </div>
 
 
 
