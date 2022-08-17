bdd0d1f129039cdcfe080e803343c8826d6f9149
kiritocode1 <kathawalearyan9@gmail.com>
2022-08-17 22:07:43 +0530

    Commit safe Offline done

--- src/Pages/BlogsPage.jsx
+++ src/Pages/BlogsPage.jsx
@@ -8,15 +8,15 @@ const Card = ({element,Name, Context, link}) => {
   return (
     <div class="card w-96 bg-primary text-primary-content overflow-hidden">
       {element}
-  <div class="card-body z-10">
-    <h2 class="card-title twitter_text text-2xl">{Name}</h2>
-    <p>{Context}</p>
-    <div class="card-actions justify-end">
-      <a className="btn " href={link} >Read Here</a>
+      <div class="card-body z-10">
+        <h2 class="card-title twitter_text text-2xl">{Name}</h2>
+        <p>{Context}</p>
+        <div class="card-actions justify-end">
+          <a className="btn " href={link} >Read Here</a>
+        </div>
+      </div>
     </div>
-  </div>
-</div>
-  )
+  );
 }
 
 
@@ -33,12 +33,24 @@ const BlogsPage = () => {
       <div className=" absolute text-accent text-6xl mono_text4 ">I Love Reading</div>
       <a.img src={booklol} alt="hehehe" className="w-40  z-10" style={{...stylie}}/>
       </div>
-      <div className="w-full items-center justify-center my-20 flex gap-6">
+      <div className="w-full items-center justify-center my-20 flex gap-6 flex-wrap">
         <Card element={<SiHashnode className="absolute text-9xl top-24 rotate-8 -left-10"/>}  Name="HashNode" Context="Im Starting to blog here as well please it would be a bunch if you follow  "    link="https://kiritocode1.hashnode.dev"/>
 
                 <Card element={<FaDev className="absolute text-9xl -top-[4rem] -rotate-[45deg] left-40"/>}  Name="Dev.to" Context="i blog once a week here and  will   continue to do so for a very long time , i love dev.to's community and interactive interface"    link="https://dev.to/kiritocode1"/>
 
                         <Card element={<SiMedium className="absolute text-9xl top-[10rem] rotate-[45deg] left-40"/>}  Name="Medium" Context="its awesome , no contest, one of the most interactive and intuitive interfaces ive ever seen , fast architexture and much more "    link="https://medium.com/@kathawalearyan"/>
+      </div>
+      <div className="w-full h-[50rem] flex flex-col ">
+        <div className="text-xl">if you want to know my future plans</div>
+        <ul class="steps steps-vertical lg:steps-horizontal">
+  <li class="step step-primary" data-content="✓">Make this site</li>
+  <li class="step step-primary">Plan a structure for devs to contribute</li>
+  <li class="step">get active contributions</li>
+          <li class="step">Receive Sponsors
+          </li>
+                    <li class="step" data-content="★">Payback to Contributors
+</li>
+</ul>
       </div>
       </div>
     
