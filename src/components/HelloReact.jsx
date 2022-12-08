388158c8c47af6a747138dabcefd1dcbff6eb8b1
kiritocode1 <kathawalearyan9@gmail.com>
2022-12-08 09:02:38 +0530

    Commit safe

---
+++ src/components/HelloReact.jsx
@@ -0,0 +1,7 @@
+const RC = () =>{ 
+    return (<h1>Hello 
+        <b>WORLD </b>
+    </h1>)
+}; 
+
+export default RC ; 
---
+++ src/components/HelloVue.vue
@@ -0,0 +1,20 @@
+
+
+
+
+<script>
+export default {
+  data() {
+    return {
+      count: 0
+    }
+  },
+  
+
+}
+</script>
+
+<template>
+  {{count}}9
+  <button @click="count++">hello</button>
+</template>
